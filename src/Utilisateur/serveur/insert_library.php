8db5c3ca70b662a806f99be4ca72d458544fa176
al3x6_ <alexisar2002@gmail.com>
2023-03-26 22:41:17 +0200

    Correction de la redirection du bouton d'enregistrement dans le module de crypto

--- src/Utilisateur/serveur/insert_library.php
+++ src/Utilisateur/serveur/insert_library.php
@@ -6,19 +6,21 @@ global $db;
 if (isset($_SESSION["texte_chiffre"])) {
     $cle = $_SESSION["cle"];
     $texte = $_SESSION["texte"];
-    $texte_chiffre= $_SESSION["texte_chiffre"];
+    $texte_chiffre = $_SESSION["texte_chiffre"];
     $login = $_SESSION["login"];
 
     $reqCle = $db->prepare("SELECT * FROM crypto WHERE login = ? and texte_chiffre = ?");
-    $reqCle->execute(array($login,$texte_chiffre));
+    $reqCle->execute(array($login, $texte_chiffre));
     $cleExiste = $reqCle->rowCount();
     if ($cleExiste == 0) {
         $insertCles = $db->prepare("INSERT INTO crypto(login,cle,texte,texte_chiffre) VALUES(?,?,?,?)");
-        $insertCles->execute(array($login,$cle, $texte, $texte_chiffre));
+        $insertCles->execute(array($login, $cle, $texte, $texte_chiffre));
         header('Location: ../SimFast-Module_Crypto.php?success');
-}
-    else{
+    } else{
         header('Location: ../SimFast-Module_Crypto.php?err');
     }
 
+}
+else{
+    header('Location: ../SimFast-Module_Crypto.php');
 }
